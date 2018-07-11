f6771705e061800573f4665ad1d295e25eb17ec3
xkaing <xkaing@yeah.net>
2018-07-11 15:11:03 +0800

    更新图表数据

--- src/component/ChartMonth.jsx
+++ src/component/ChartMonth.jsx
@@ -10,6 +10,7 @@ import 'echarts/lib/component/title';
 import 'echarts/lib/component/tooltip';
 import 'echarts/lib/component/legend';
 import 'echarts/lib/component/grid';
+// import 'echarts/lib/component/dataZoom';
 
 const styles = {
   card: {
@@ -24,7 +25,7 @@ class ChartMonth extends Component {
     let myChart = echarts.init(document.getElementById('pieChart'));
     myChart.setOption({
       title: {
-        text: '每日开销统计'
+        text: '7月开销统计'
       },
       legend: {
         data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎', '百度', '谷歌', '必应', '其他']
@@ -32,7 +33,7 @@ class ChartMonth extends Component {
       grid: {
         left: '3%',
         right: '4%',
-        bottom: '3%',
+        bottom: '1%',
         containLabel: true
       },
       tooltip: {
@@ -43,7 +44,7 @@ class ChartMonth extends Component {
       },
       xAxis: {
         type: 'category',
-        data: ['1号', '2号', '3号', '4号', '5号', '6号', '7号'],
+        data: ['1号', '2号', '3号', '4号', '5号', '6号', '7号', '8号', '9号', '10号', '11号', '12号', '13号', '14号', '15号', '16号', '17号', '18号', '19号', '20号', '21号', '22号', '23号', '24号', '25号', '26号', '27号', '28号', '29号', '30号', '31号'],
         axisTick: {
           alignWithLabel: true
         }
@@ -55,17 +56,17 @@ class ChartMonth extends Component {
         {
           name: '邮件营销',
           type: 'bar',
-          stack: '广告',
+          stack: '日常开销',
           data: [120, 132, 101, 134, 90, 230, 210]
         },{
           name: '联盟广告',
           type: 'bar',
-          stack: '广告',
+          stack: '日常开销',
           data: [220, 182, 191, 234, 290, 330, 310]
         },{
           name: '视频广告',
           type: 'bar',
-          stack: '广告',
+          stack: '日常开销',
           data: [150, 232, 201, 154, 190, 330, 410]
         },
       ]
@@ -77,7 +78,7 @@ class ChartMonth extends Component {
     return (
       <div>
         <Card className={classes.card}>
-          <CardContent style={{ width: 1000, height: 400 }} id="pieChart" />
+          <CardContent style={{ width: 1000, height: 450 }} id="pieChart" />
         </Card>
       </div>
     )
