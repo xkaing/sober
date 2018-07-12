fc0adac4e889cccf294fa5cdb0983cea559bc2f0
xkaing <xkaing@yeah.net>
2018-07-12 09:57:17 +0800

    更新数据

--- src/view/finance.jsx
+++ src/view/finance.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
 import ChartMonth from '../component/ChartMonth'
 
 const styles = theme => ({
