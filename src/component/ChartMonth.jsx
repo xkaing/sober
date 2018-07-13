6d1cff2a789b16f1a72748e630167e926f52a0e8
xkaing <xkaing@yeah.net>
2018-07-13 10:24:41 +0800

    更新图表数据

--- src/component/ChartMonth.jsx
+++ src/component/ChartMonth.jsx
@@ -25,7 +25,7 @@ class ChartMonth extends Component {
     let myChart = echarts.init(document.getElementById('pieChart'));
     myChart.setOption({
       title: {
-        text: '7月开销统计'
+        text: '7月日常开销'
       },
       legend: {
         data: ['早餐', '午餐', '零食', '衣着', '信用卡', '外债', '京东金融', '公交卡', '支付宝群收款', '阿里云服务', '饿了么会员']
@@ -44,7 +44,7 @@ class ChartMonth extends Component {
       },
       xAxis: {
         type: 'category',
-        data: ['1号', '2号', '3号', '4号', '5号', '6号', '7号', '8号', '9号', '10号', '11号', '12号', '13号', '14号', '15号', '16号', '17号', '18号', '19号', '20号', '21号', '22号', '23号', '24号', '25号', '26号', '27号', '28号', '29号', '30号', '31号'],
+        data: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'],
         axisTick: {
           alignWithLabel: true
         }
@@ -57,7 +57,7 @@ class ChartMonth extends Component {
           name: '早餐',
           type: 'bar',
           stack: '开销',
-          data: [0, 8, 0, 7, 7, 7, 0, 0, 0, 0, 0]
+          data: [0, 8, 0, 7, 7, 7, 0, 0, 0, 0, 0, 5.5]
         },{
           name: '午餐',
           type: 'bar',
@@ -74,41 +74,11 @@ class ChartMonth extends Component {
           stack: '开销',
           data: [0, 0, 0, 144, 0, 0, 0, 0, 0, 0, 0]
         },{
-          name: '信用卡',
-          type: 'bar',
-          stack: '开销',
-          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 4801, 0]
-        },{
-          name: '外债',
-          type: 'bar',
-          stack: '开销',
-          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 4863, 0]
-        }, {
-          name: '京东金融',
-          type: 'bar',
-          stack: '开销',
-          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 990, 0]
-        }, {
           name: '公交卡',
           type: 'bar',
           stack: '开销',
           data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 0]
-        }, {
-          name: '支付宝群收款',
-          type: 'bar',
-          stack: '开销',
-          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 204, 0]
-        }, {
-          name: '阿里云服务',
-          type: 'bar',
-          stack: '开销',
-          data: [0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0]
-        }, {
-          name: '饿了么会员',
-          type: 'bar',
-          stack: '开销',
-          data: [0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0]
-        },
+        }
       ]
     });
   }
