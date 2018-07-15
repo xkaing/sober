debde3b5a6d17e685539559f8f027722fa720f8c
xkaing <xkaing@yeah.net>
2018-07-15 23:50:10 +0800

    修改组件

---
+++ src/component/TotalAssets.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+// import Typography from '@material-ui/core/Typography';
+
+
+class TotalAssets extends Component {
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default TotalAssets
